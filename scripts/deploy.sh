99d034a46598a55f0892081e7665bd1f4fcd3d12
Josh Chorlton <jchorlton@gmail.com>
2020-10-06 19:48:56 -0700

    add deploy script and s3 site in tf

---
+++ scripts/deploy.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+set -euo pipefail
+
+docker run -it --rm \
+    -v "$(pwd)/frontend":/securesend:ro \
+    -v "$(pwd)/build/frontend":/securesend/build \
+    -w /securesend \
+    node:14.12 \
+    yarn build
+
+docker run -it --rm \
+    -v "$(pwd)/build/frontend":/contents \
+    -w /contents \
+    --env-file .aws_creds \
+    amazon/aws-cli \
+    s3 cp --recursive . s3://securesend-site/
--- terraform/main.tf
+++ terraform/main.tf
@@ -15,3 +15,29 @@ resource "aws_dynamodb_table" "securesend-table" {
     enabled        = true
   }
 }
+
+resource "aws_s3_bucket" "site" {
+  bucket = "securesend-site"
+  acl    = "public-read"
+
+  website {
+    index_document = "index.html"
+  }
+}
+
+resource "aws_s3_bucket_policy" "site" {
+  bucket = aws_s3_bucket.site.id
+  policy = data.aws_iam_policy_document.site_public_access.json
+}
+
+data "aws_iam_policy_document" "site_public_access" {
+  statement {
+    actions   = ["s3:Get*", "s3:List*"]
+    resources = ["${aws_s3_bucket.site.arn}/*"]
+
+    principals {
+      type        = "AWS"
+      identifiers = ["*"]
+    }
+  }
+}
