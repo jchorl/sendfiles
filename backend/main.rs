873913ff810fd38fb9588a22d2bebbfed5ebcc30
Josh Chorlton <jchorlton@gmail.com>
2020-10-07 19:43:59 -0700

    refactor into an api

--- backend/main.rs
+++ backend/main.rs
@@ -17,10 +17,15 @@ type Error = Box<dyn std::error::Error + Send + Sync + 'static>;
 #[lambda(http)]
 #[tokio::main]
 async fn main(request: Request, _: Context) -> Result<impl IntoResponse, Error> {
+    let api = Api {
+        db_client: Box::new(DynamoDbClient::new(Region::UsWest2).with_retries(Policy::default())),
+        secure_send_dynamo_table: "SecuresendTransfersTest".to_string(),
+    };
+
     match request.uri().path() {
-        "/transfers" => post_transfer_handler(request).await,
-        "/transfer" => get_transfer_handler(request).await,
-        _ => not_found(request),
+        "/transfers" => api.post_transfer_handler(request).await,
+        "/transfer" => api.get_transfer_handler(request).await,
+        _ => api.not_found(request),
     }
 }
 
@@ -39,66 +44,72 @@ struct TransferDetails {
     valid_until: DateTime<Utc>,
 }
 
-async fn post_transfer_handler(request: Request) -> Result<Response<String>, Error> {
-    let body = request.into_body();
-    let mut details: TransferDetails = serde_json::from_slice(body.as_ref())?;
-    details.id = Some(Uuid::new_v4().to_hyphenated().to_string());
-
-    let client = DynamoDbClient::new(Region::UsWest2).with_retries(Policy::default());
-    let input = PutItemInput {
-        table_name: "SecuresendTransfersTest".to_string(),
-        item: details.clone().into(),
-        ..PutItemInput::default()
-    };
-
-    let result = client.put_item(input).await;
-    if let Err(e) = result {
-        println!("error writing details to db: {}", e);
-        return Ok(Response::builder()
-            .status(500)
-            .body("error saving transfer".to_string())
-            .unwrap());
-    }
-
-    let resp = serde_json::to_string(&details)?;
-    Ok(Response::new(resp))
+struct Api {
+    db_client: Box<dyn DynamoDb + Send + Sync>,
+    secure_send_dynamo_table: String,
 }
 
-async fn get_transfer_handler(request: Request) -> Result<Response<String>, Error> {
-    let client = DynamoDbClient::new(Region::UsWest2).with_retries(Policy::default());
-    let details_id = request
-        .query_string_parameters()
-        .get("id")
-        .unwrap()
-        .to_string();
+impl Api {
+    async fn post_transfer_handler(&self, request: Request) -> Result<Response<String>, Error> {
+        let body = request.into_body();
+        let mut details: TransferDetails = serde_json::from_slice(body.as_ref())?;
+        details.id = Some(Uuid::new_v4().to_hyphenated().to_string());
 
-    let details_key = TransferDetailsKey {
-        id: Some(details_id),
-    };
+        let input = PutItemInput {
+            table_name: self.secure_send_dynamo_table.clone(),
+            item: details.clone().into(),
+            ..PutItemInput::default()
+        };
 
-    let result = client
-        .get_item(GetItemInput {
-            table_name: "SecuresendTransfersTest".to_string(),
-            key: details_key.into(),
-            ..GetItemInput::default()
-        })
-        .await;
-    if let Err(e) = result {
-        println!("error getting details from db: {}", e);
-        return Ok(Response::builder()
+        let result = self.db_client.put_item(input).await;
+        if let Err(e) = result {
+            println!("error writing details to db: {}", e);
+            return Ok(Response::builder()
+                .status(500)
+                .body("error saving transfer".to_string())
+                .unwrap());
+        }
+
+        let resp = serde_json::to_string(&details)?;
+        Ok(Response::new(resp))
+    }
+
+    async fn get_transfer_handler(&self, request: Request) -> Result<Response<String>, Error> {
+        let details_id = request
+            .query_string_parameters()
+            .get("id")
+            .unwrap()
+            .to_string();
+
+        let details_key = TransferDetailsKey {
+            id: Some(details_id),
+        };
+
+        let result = self
+            .db_client
+            .get_item(GetItemInput {
+                table_name: self.secure_send_dynamo_table.clone(),
+                key: details_key.into(),
+                ..GetItemInput::default()
+            })
+            .await;
+        if let Err(e) = result {
+            println!("error getting details from db: {}", e);
+            return Ok(Response::builder()
+                .status(404)
+                .body("transfer not found".to_string())
+                .unwrap());
+        }
+
+        let details = TransferDetails::from_attrs(result.unwrap().item.unwrap()).unwrap();
+        let resp = serde_json::to_string(&details)?;
+        Ok(Response::new(resp))
+    }
+
+    fn not_found(&self, _request: Request) -> Result<Response<String>, Error> {
+        Ok(Response::builder()
             .status(404)
-            .body("transfer not found".to_string())
-            .unwrap());
+            .body("".to_string())
+            .unwrap())
     }
-
-    let details = TransferDetails::from_attrs(result.unwrap().item.unwrap()).unwrap();
-    let resp = serde_json::to_string(&details)?;
-    Ok(Response::new(resp))
-}
-
-fn not_found(_request: Request) -> Result<Response<String>, Error> {
-    Ok(Response::builder()
-        .status(404)
-        .body("".to_string())
-        .unwrap())
 }
