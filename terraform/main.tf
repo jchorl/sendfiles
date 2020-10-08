143c414bad5b8df61a978b8332533369febfe22e
Josh Chorlton <jchorlton@gmail.com>
2020-10-07 21:05:42 -0700

    factoring backend into multiple lambdas

--- terraform/main.tf
+++ terraform/main.tf
@@ -16,6 +16,24 @@ resource "aws_dynamodb_table" "securesend-table" {
   }
 }
 
+resource "aws_dynamodb_table" "connections-table" {
+  name           = "connections"
+  billing_mode   = "PROVISIONED"
+  read_capacity  = 20
+  write_capacity = 20
+  hash_key       = "connection_id"  # this should include sender/receiver
+
+  attribute {
+    name = "connection_id"
+    type = "S"
+  }
+
+  ttl {
+    attribute_name = "valid_until"
+    enabled        = true
+  }
+}
+
 resource "aws_s3_bucket" "site" {
   bucket = "securesend-site"
   acl    = "public-read"
