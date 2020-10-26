3015f467765d9b6f87c729be27a8870fb86e3d15
Josh Chorlton <jchorlton@gmail.com>
2020-10-25 17:51:59 -0700

    lint

--- frontend/src/Crypto.js
+++ frontend/src/Crypto.js
@@ -43,14 +43,22 @@ export async function decryptMessage(ciphertext, key, password) {
 
 export async function exportKeyAsBase64(key) {
   const exportedKey = await crypto.subtle.exportKey("raw", key);
-  const chars = String.fromCharCode(...new Uint8Array(exportedKey))
+  const chars = String.fromCharCode(...new Uint8Array(exportedKey));
   return btoa(chars);
 }
 
 export async function importKeyFromBase64(encoded) {
   const b64DecodedKey = atob(encoded);
-  const charCodes = b64DecodedKey.split('').map((c, idx) => b64DecodedKey.charCodeAt(idx))
+  const charCodes = b64DecodedKey
+    .split("")
+    .map((c, idx) => b64DecodedKey.charCodeAt(idx));
   const decodedKey = new Uint8Array(charCodes);
-  const privateKey = await crypto.subtle.importKey("raw", decodedKey, algo, false, ["decrypt"]);
+  const privateKey = await crypto.subtle.importKey(
+    "raw",
+    decodedKey,
+    algo,
+    false,
+    ["decrypt"]
+  );
   return privateKey;
 }
