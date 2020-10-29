a5cd7ad3555663ce62803a4825f82f7bc0a0c5af
Josh Chorlton <jchorlton@gmail.com>
2020-10-29 09:54:28 -0700

    move frontend to public endpoints

--- frontend/src/Config.js
+++ frontend/src/Config.js
@@ -1,6 +1,6 @@
 export default {
-  TRANSFER_API: "https://mbzejo2pjf.execute-api.us-west-2.amazonaws.com/prod",
-  COORD_API: "wss://bzuswxfrb1.execute-api.us-west-2.amazonaws.com/prod",
+  TRANSFER_API: "https://transfers.sendfiles.dev",
+  COORD_API: "wss://coord.sendfiles.dev",
   FILE_VALID_HOURS: 1,
   STUN_SERVER: "stun:stun.l.google.com:19302", // TODO switch to stuns when firefox supports it
 };
