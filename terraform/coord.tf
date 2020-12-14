47c7ccfbc6332327bbfa85ece3dfad451b2b6119
Josh Chorlton <jchorlton@gmail.com>
2020-12-14 15:28:28 +0000

    lower read/write capacity

--- terraform/coord.tf
+++ terraform/coord.tf
@@ -1,8 +1,8 @@
 resource "aws_dynamodb_table" "offers_table" {
   name           = "OffersTest"
   billing_mode   = "PROVISIONED"
-  read_capacity  = 20
-  write_capacity = 20
+  read_capacity  = 2
+  write_capacity = 2
   hash_key       = "transfer_id"
 
   attribute {
--- terraform/transfers.tf
+++ terraform/transfers.tf
@@ -1,8 +1,8 @@
 resource "aws_dynamodb_table" "securesend_table" {
   name           = "SecuresendTransfersTest"
   billing_mode   = "PROVISIONED"
-  read_capacity  = 20
-  write_capacity = 20
+  read_capacity  = 2
+  write_capacity = 2
   hash_key       = "id"
 
   attribute {
