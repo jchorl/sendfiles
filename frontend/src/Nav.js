42c8287806c43a85cbb07c4814cbaa1625a4e8a3
Josh Chorlton <jchorlton@gmail.com>
2020-12-15 05:30:14 +0000

    optimize nav for mobile

--- frontend/src/Nav.js
+++ frontend/src/Nav.js
@@ -6,14 +6,15 @@ function Nav() {
     <div className="nav">
       <a href="/">Home</a>
       <span className="divider">|</span>
-      <a href="/">Transfer Files</a>
-      <span className="divider">|</span>
+      <a href="/" className="mobile-hidden">Transfer Files</a>
+      <span className="divider mobile-hidden">|</span>
       <a href="/about">About</a>
-      <span className="divider">|</span>
+      <span className="divider mobile-hidden">|</span>
       <a
         href="https://github.com/jchorl/sendfiles"
         target="_blank"
         rel="noopener noreferrer"
+        className="mobile-hidden"
       >
         Source
       </a>
