64de423f985814718f41268d7c4d1c81a452c8ad
Josh Chorlton <jchorlton@gmail.com>
2024-01-13 05:51:53 +0000

    transfers builds

--- backend/transfers/src/main.rs
+++ backend/transfers/src/main.rs
@@ -1,40 +1,41 @@
+use aws_config::BehaviorVersion;
+use aws_sdk_dynamodb as dynamodb;
 use chrono::{DateTime, Utc};
-use dynomite::{
-    dynamodb::{DynamoDb, DynamoDbClient, GetItemInput, PutItemInput},
-    retry::Policy,
-    FromAttributes, Item, Retries,
-};
+use dynamodb::types::AttributeValue;
 use http::method::Method;
-use lambda_http::{
-    lambda::{lambda, Context},
-    IntoResponse, Request, RequestExt, Response,
-};
-use rusoto_core::Region;
+use lambda_http::{http::StatusCode, Request, RequestExt, Response};
+use lambda_http::{service_fn, Error};
+use log::error;
 use serde_derive::{Deserialize, Serialize};
+use std::collections::HashMap;
 use uuid::Uuid;
 
-type Error = Box<dyn std::error::Error + Send + Sync + 'static>;
-
-#[lambda(http)]
 #[tokio::main]
-async fn main(request: Request, _: Context) -> Result<impl IntoResponse, Error> {
+async fn main() -> Result<(), Error> {
+    let config = aws_config::load_defaults(BehaviorVersion::latest()).await;
+    let client = aws_sdk_dynamodb::Client::new(&config);
+
     let api = Api {
-        db_client: Box::new(DynamoDbClient::new(Region::default()).with_retries(Policy::default())),
+        dynamodb_client: client,
         transfers_table: "Transfers".to_string(),
     };
 
-    match request.method() {
-        &Method::POST => api.post_transfer_handler(request).await,
-        &Method::GET => api.get_transfer_handler(request).await,
-        _ => api.not_found(request),
-    }
+    lambda_http::run(service_fn(|request: Request| async {
+        match *request.method() {
+            Method::POST => api.post_transfer_handler(request).await,
+            Method::GET => api.get_transfer_handler(request).await,
+            _ => api.not_found(request),
+        }
+    }))
+    .await?;
+
+    Ok(())
 }
 
-#[derive(Serialize, Deserialize, Item, Clone)]
+#[derive(Serialize, Deserialize, Clone)]
 #[serde(rename_all = "camelCase")]
 struct TransferDetails {
     #[serde(skip_deserializing)]
-    #[dynomite(partition_key)]
     id: String,
     file_name: String,
     content_length_bytes: u32,
@@ -42,8 +43,36 @@ struct TransferDetails {
     valid_until: DateTime<Utc>,
 }
 
+impl From<&HashMap<String, AttributeValue>> for TransferDetails {
+    fn from(value: &HashMap<String, AttributeValue>) -> Self {
+        TransferDetails {
+            id: value.get("id").unwrap().as_s().unwrap().to_owned(),
+            file_name: value.get("file_name").unwrap().as_s().unwrap().to_owned(),
+            content_length_bytes: value
+                .get("content_length_bytes")
+                .unwrap()
+                .as_n()
+                .unwrap()
+                .parse::<u32>()
+                .unwrap(),
+            private_key: value.get("private_key").unwrap().as_s().unwrap().to_owned(),
+            valid_until: DateTime::<Utc>::from_timestamp(
+                value
+                    .get("valid_until")
+                    .unwrap()
+                    .as_n()
+                    .unwrap()
+                    .parse::<i64>()
+                    .unwrap(),
+                0,
+            )
+            .unwrap(),
+        }
+    }
+}
+
 struct Api {
-    db_client: Box<dyn DynamoDb + Send + Sync>,
+    dynamodb_client: aws_sdk_dynamodb::client::Client,
     transfers_table: String,
 }
 
@@ -51,64 +80,77 @@ impl Api {
     async fn post_transfer_handler(&self, request: Request) -> Result<Response<String>, Error> {
         let body = request.into_body();
         let mut details: TransferDetails = serde_json::from_slice(body.as_ref())?;
-        details.id = Uuid::new_v4().to_hyphenated().to_string();
+        details.id = Uuid::new_v4().as_hyphenated().to_string();
 
-        let input = PutItemInput {
-            table_name: self.transfers_table.clone(),
-            item: details.clone().into(),
-            ..PutItemInput::default()
+        let dynamodb_request = self
+            .dynamodb_client
+            .put_item()
+            .table_name(self.transfers_table.to_owned())
+            .item("id", AttributeValue::S(details.id))
+            .item("file_name", AttributeValue::S(details.file_name))
+            .item(
+                "content_length_bytes",
+                AttributeValue::N(details.content_length_bytes.to_string()),
+            )
+            .item("private_key", AttributeValue::S(details.private_key))
+            .item(
+                "valid_until",
+                AttributeValue::N(details.valid_until.timestamp().to_string()),
+            );
+
+        let resp = match dynamodb_request.send().await {
+            Ok(result) => TransferDetails::from(result.attributes().unwrap()),
+            Err(e) => {
+                error!("writing to db: {}", e);
+                return Ok(Response::builder()
+                    .status(StatusCode::INTERNAL_SERVER_ERROR)
+                    .body("error saving transfer".to_string())
+                    .unwrap());
+            }
         };
 
-        let result = self.db_client.put_item(input).await;
-        if let Err(e) = result {
-            println!("error writing details to db: {}", e);
-            return Ok(Response::builder()
-                .status(500)
-                .body("error saving transfer".to_string())
-                .unwrap());
-        }
-
-        let resp = serde_json::to_string(&details)?;
-        Ok(Response::new(resp))
+        Ok(Response::new(serde_json::to_string(&resp).unwrap()))
     }
 
     async fn get_transfer_handler(&self, request: Request) -> Result<Response<String>, Error> {
-        let details_id = request
-            .query_string_parameters()
-            .get("id")
-            .unwrap()
-            .to_string();
-
-        let details_key = TransferDetailsKey {
-            id: details_id.clone(),
+        let details_id = match request
+            .query_string_parameters_ref()
+            .and_then(|params| params.first("id"))
+        {
+            Some(id) => id.to_owned(),
+            None => {
+                return Ok(Response::builder()
+                    .status(StatusCode::BAD_REQUEST)
+                    .body("id query paramater must be passed".to_owned())
+                    .unwrap())
+            }
         };
 
-        let result = self
-            .db_client
-            .get_item(GetItemInput {
-                table_name: self.transfers_table.clone(),
-                key: details_key.into(),
-                ..GetItemInput::default()
-            })
-            .await;
-        if let Err(e) = result {
-            println!("error getting details from db: {}", e);
-            return Ok(Response::builder()
-                .status(404)
-                .body("transfer not found".to_string())
-                .unwrap());
-        }
+        let dynamodb_request = self
+            .dynamodb_client
+            .get_item()
+            .table_name(self.transfers_table.to_owned())
+            .key("id", AttributeValue::S(details_id));
 
-        let item = result.unwrap().item;
-        if let None = item {
-            println!("item not found: {}", details_id);
-            return Ok(Response::builder()
-                .status(404)
-                .body("transfer not found".to_string())
-                .unwrap());
-        }
+        let details = match dynamodb_request.send().await {
+            Ok(resp) => match resp.item() {
+                Some(hm) => TransferDetails::from(hm),
+                None => {
+                    return Ok(Response::builder()
+                        .status(404)
+                        .body("transfer not found".to_string())
+                        .unwrap())
+                }
+            },
+            Err(e) => {
+                error!("error getting details from db: {}", e);
+                return Ok(Response::builder()
+                    .status(404)
+                    .body("transfer not found".to_string())
+                    .unwrap());
+            }
+        };
 
-        let details = TransferDetails::from_attrs(item.unwrap()).unwrap();
         let resp = serde_json::to_string(&details)?;
         Ok(Response::new(resp))
     }
@@ -116,7 +158,7 @@ impl Api {
     fn not_found(&self, _request: Request) -> Result<Response<String>, Error> {
         Ok(Response::builder()
             .status(404)
-            .body("".to_string())
+            .body("route not found".to_string())
             .unwrap())
     }
 }
