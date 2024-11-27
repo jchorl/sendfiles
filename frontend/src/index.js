bb544f4365028d51414badf84e205e460cdd8b09
Josh Chorlton <josh@bugseq.com>
2024-11-27 18:35:12 +0000

    lint

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,14 +1,14 @@
 import React from "react";
-import * as ReactDOMClient from 'react-dom/client';
+import * as ReactDOMClient from "react-dom/client";
 import "./index.css";
 import App from "./App";
 
-const container = document.getElementById('root');
+const container = document.getElementById("root");
 
-const root = ReactDOMClient.createRoot(container)
+const root = ReactDOMClient.createRoot(container);
 
 root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-)
+);
